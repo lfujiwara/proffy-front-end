1c20f133074d5db7d71e2d8ec49562eecbb0ebf4
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2020-09-20 16:53:31 -0300

    Set up theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
-import { Text } from "@chakra-ui/core";
+import { Box, Text } from "@chakra-ui/core";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Box
+      backgroundColor="another_purple"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
       <Head>
         <title>Proffy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Text>Hello, world</Text>
-    </div>
+      <Text color="texts_in_purple_title">Hello, world!</Text>
+    </Box>
   );
 }
