5011de6cd15ff493b22b69a6aa97aa1cf4a44270
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2020-09-20 15:44:33 -0300

    Set up Chakra UI & TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { CSSReset, ThemeProvider } from "@chakra-ui/core";
+import type { AppProps } from "next/app";
+import theme from "../styles/theme";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
